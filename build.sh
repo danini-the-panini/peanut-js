3cf515838a22fef92426631289c90632d3d05da4
Danielle Smith <code@danini.dev>
2025-04-23 12:13:33 +0200

    style, build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+emcc main.c -o index.html -O3 --shell-file html_templates/shell_minimal.html -s NO_EXIT_RUNTIME=1 -s "EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'HEAPU8']" -s EXPORTED_FUNCTIONS='["_malloc", "_main"]' -s USE_SDL=2 -sASSERTIONS --js-library=library.js
